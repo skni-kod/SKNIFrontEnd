2ebad60b3a6a868e7b24848fe25605c6c18103b4
tearth <tearthdev@gmail.com>
2019-02-13 15:30:14 +0100

    Add vuetify package.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false;
 
@@ -8,3 +11,7 @@ new Vue({
   router,
   render: (h) => h(App),
 }).$mount('#app');
+
+Vue.use(Vuetify, {
+  iconfont: 'md'
+});
