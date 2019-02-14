434ea411d09f4089b8f612a9e1eb6f953d8c76d5
AzuxDario <pawel_mibor95@tlen.pl>
2019-02-14 18:24:44 +0100

    changes in comments

--- src/router.ts
+++ src/router.ts
@@ -21,11 +21,11 @@ export default new Router({
     },
     {
       path: '/sections',
-      name: 'about',
+      name: 'sections',
       // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
+      // this generates a separate chunk (sections.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Sections.vue'),
+      component: () => import(/* webpackChunkName: "sections" */ './views/Sections.vue'),
     },
   ],
 });
