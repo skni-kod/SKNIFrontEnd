e4a8055d6bb82c6df68f62f960a2967ba2625b4c
Paweł Dziedzic <blavikenn@gmail.com>
2019-02-17 17:11:16 +0100

    test buttons in Login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,31 +1,36 @@
 <template>
   <v-container>
-    <v-layout row class="text-xs-center">
-      <v-flex xs4 class="grey lighten-4">
-        <v-container class="text-xs-center">
+    <v-layout row class='text-xs-center'>
+      <v-flex xs4 class='grey lighten-4'>
+        <v-container class='text-xs-center'>
           <v-card flat>
             <v-card-title primary-title>
               <h4>Login</h4>
             </v-card-title>
             <v-form>
-              <v-text-field prepend-icon="person" name="Username" label="Username" v-model="login"></v-text-field>
+              <v-text-field prepend-icon='person' name='Username' label='Username' v-model='login'></v-text-field>
               <v-text-field
-                prepend-icon="lock"
-                name="Password"
-                label="Password"
-                type="password"
-                v-model="password"
+                prepend-icon='lock'
+                name='Password'
+                label='Password'
+                type='password'
+                v-model='password'
               ></v-text-field>
               <v-card-actions>
-                <v-btn primary large block @click="loginUser()">Login</v-btn>
+                <v-btn primary large block @click='loginUser()'>Login</v-btn>
               </v-card-actions>
               <v-card-actions>
-                <v-btn primary large block @click="logout()">Logout</v-btn>
+                <v-btn primary large block @click='logout()'>Logout</v-btn>
+              </v-card-actions>
+              <v-card-actions>
+                <v-btn primary large block @click='refresh()'>Token</v-btn>
+              </v-card-actions>
+              <v-card-actions>
+                <v-btn primary large block @click='testToken()'>Add thing</v-btn>
               </v-card-actions>
             </v-form>
 
-            <div v-if="isLogged()"> Kappa </div>
-
+            <div v-if='isLogged()'>Kappa</div>
           </v-card>
         </v-container>
       </v-flex>
@@ -33,26 +38,54 @@
   </v-container>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
-import { login, logout, isLoggedIn } from "../helpers/auth";
+<script lang='ts'>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import {
+  login,
+  logout,
+  isLoggedIn,
+  refreshToken,
+  createAuthHeader,
+} from '../helpers/auth';
+import Axios from 'axios';
 
 @Component
 export default class Login extends Vue {
-  login: string = "";
-  password: string = "";
+  private login: string = '';
+  private password: string = '';
 
-  loginUser(): void {
-    login(this.login, this.password);
+  private async loginUser() {
+    const res = await login(this.login, this.password);
+    if (res.status === 200) {
+      this.$forceUpdate();
+    }
   }
 
-  logout(): void {
+  private logout(): void {
     logout();
+    this.$forceUpdate();
   }
 
-  isLogged() : boolean {
-    return isLoggedIn();
+  private isLogged(): boolean {
+    const a = isLoggedIn();
+    return a;
+  }
+
+  private refresh() {
+    refreshToken();
+  }
+
+  private testToken() {
+    const header = createAuthHeader();
+
+    Axios.post(
+      'http://localhost:8000/tags/',
+      { name: 'Kappa' },
+      { headers: header },
+    ).then((res) => {
+      console.log(res.status + ' -> Refresh token');
+    });
   }
 }
 </script>
