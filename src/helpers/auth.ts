1a055c587f4fa679efe186cbbd360e11bee544bd
Paweł Dziedzic <blavikenn@gmail.com>
2019-02-17 16:25:22 +0100

    Change auth functions to reflect changes in API, added refreshToken and createAuthHeader functions

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,17 +1,47 @@
 import axios from 'axios';
 
 
+const accessTokenName: string = 'access_token';
+const refreshTokenName: string = 'refresh_token';
+
 export function login(username: string, password: string) {
-    axios.post('http://localhost:8000/obtain-token/', { "username": username, "password": password,})
+    axios.post('http://localhost:8000/obtain-token/', { "username": username, "password": password, })
         .then(res => {
-            localStorage.setItem('token', res.data['token']);
+            localStorage.setItem(accessTokenName, res.data.access);
+            localStorage.setItem(refreshTokenName, res.data.refresh);
         });
 }
 
 export function logout() {
-    localStorage.removeItem('token');
+    localStorage.removeItem(accessTokenName);
 }
 
-export function isLoggedIn() : boolean {
-    return localStorage.getItem('token') != null;
+export function refreshToken() {
+    const token = localStorage.getItem(refreshTokenName);
+
+    if (token == null) {
+        return;
+    }
+
+    axios.post('http://localhost:8000/refresh-token/', { "refresh": token })
+        .then(res => {
+            localStorage.setItem(accessTokenName, res.data.access);
+        });
+}
+
+export function isLoggedIn(): boolean {
+    let token = localStorage.getItem(accessTokenName);
+    return token !== null;
+}
+
+export function createAuthHeader() {
+    const token = localStorage.getItem(accessTokenName);
+
+    if (token == null) {
+        return;
+    }
+
+    return {
+        'Authorization': 'Bearer ' + token,
+    };
 }
