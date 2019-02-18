5337c3a7a31088066a1d5e5db60250490f506562
tearth <tearthdev@gmail.com>
2019-02-18 17:51:15 +0100

    Axios refactoring.

--- src/services/ArticlesService.ts
+++ src/services/ArticlesService.ts
@@ -5,24 +5,40 @@ export class ArticlesService {
     private axios = require('axios');
 
     public async getAllArticles(): Promise<ArticleModel[]> {
-        return (await this.axios('http://localhost:8000/articles/?format=json')).data;
+        return (await this.axios('http://localhost:8000/articles/', {
+            params: {
+                format: 'json',
+            },
+        })).data;
     }
 
     public async getArticles(pageNumber: number, pageSize: number): Promise<PaginationContainer<ArticleModel>> {
-        return (await this.axios('http://localhost:8000/articles/?format=json&' +
-            '&offset=' + (pageNumber - 1) * pageSize +
-            '&limit=' + pageSize)).data;
+        return (await this.axios('http://localhost:8000/articles/', {
+            params: {
+                format: 'json',
+                offset: (pageNumber - 1) * pageSize,
+                limit: pageSize,
+            },
+        })).data;
     }
 
     // tslint:disable-next-line:max-line-length
     public async getArticlesWithTag(tag: string, pageNumber: number, pageSize: number): Promise<PaginationContainer<ArticleModel>> {
-        return (await this.axios('http://localhost:8000/articles/?tagname=' + tag +
-            '&format=json&' +
-            '&offset=' + (pageNumber - 1) * pageSize +
-            '&limit=' + pageSize)).data;
+        return (await this.axios('http://localhost:8000/articles/', {
+            params: {
+                tagname: tag,
+                format: 'json',
+                offset: (pageNumber - 1) * pageSize,
+                limit: pageSize,
+            },
+        })).data;
     }
 
     public async getArticle(id: number): Promise<ArticleModel> {
-        return (await this.axios('http://localhost:8000/articles/' + id + '?format=json')).data;
+        return (await this.axios('http://localhost:8000/articles/' + id, {
+            params: {
+                format: 'json',
+            },
+        })).data;
     }
 }
