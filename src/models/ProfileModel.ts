ad99e1429257602fed349fd9f0bf465f25236566
AzuxDario <pawel_mibor95@tlen.pl>
2019-02-20 17:44:44 +0100

    Add ProfileModel and change UserModel

---
+++ src/models/ProfileModel.ts
@@ -0,0 +1,6 @@
+import { UserModel } from "@/models/UserModel";
+
+export class ProfileModel {
+    public id!: number;
+    public user!: UserModel;
+}
--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,4 +1,9 @@
 export class UserModel {
     public id!: number;
     public username!: string;
+    public email!: string;
+    public groups!: number[];
+    public profile!: number;
+    public first_name!: string;
+    public last_name!: string;
 }
