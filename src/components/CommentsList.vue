bca88b0501fddc211b5082081d90201f602501d3
tearth <tearthdev@gmail.com>
2019-02-20 18:01:13 +0100

    Update layout for comments.

--- src/components/CommentsList.vue
+++ src/components/CommentsList.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    <div
-      v-for="comment in comments"
-      :key="comment.id"
-      style="border: 1px solid gray"
-    >[{{comment.creation_date | moment("DD-MM-YYYY")}} {{comment.creation_date | moment("HH:mm:SS")}}] {{comment.user.user.username}} napisał: {{comment.text}}</div>
+    <h3 class="text-xs-left" style="margin-top: 20px;">Komentarze:</h3>
+    <div v-for="comment in comments" :key="comment.id">
+      <v-card style="margin-top: 5px">
+        <v-card-title primary-title>
+          <div>
+            <div>
+              <v-icon>person</v-icon> {{comment.user.user.username}}
+              <v-icon>calendar_today</v-icon> {{comment.creation_date | moment("DD-MM-YYYY HH:mm:SS")}}
+            </div>
+            <div class="text-xs-left" style="margin-top: 10px;">
+              {{comment.text}}
+            </div>
+          </div>
+        </v-card-title>
+      </v-card>
+    </div>
   </div>
 </template>
 
