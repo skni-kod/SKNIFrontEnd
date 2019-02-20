5a9acad7c74764540886425389d41e21822b331f
AzuxDario <pawel_mibor95@tlen.pl>
2019-02-20 17:14:28 +0100

    Add pagination to hardware

--- src/services/HardwaresService.ts
+++ src/services/HardwaresService.ts
@@ -1,9 +1,27 @@
 import {HardwareModel} from '@/models/HardwareModel';
+import { PaginationContainer } from '@/models/PaginationContainer';
 
 export class HardwaresService {
     private axios = require('axios');
 
     public async getAllHardware(): Promise<HardwareModel[]> {
-        return (await this.axios('http://localhost:8000/hardwares/')).data;
+        return (await this.axios('http://localhost:8000/hardwares/', {
+            params: {
+                format: 'json',
+            },
+        })).data;
+    }
+
+    public async getHardwareByPage(pageNumber: number, pageSize: number): Promise<PaginationContainer<HardwareModel>> {
+        const data = (await this.axios('http://localhost:8000/hardwares/', {
+            params: {
+                format: 'json',
+                offset: (pageNumber - 1) * pageSize,
+                limit: pageSize,
+            },
+        })).data as PaginationContainer<HardwareModel>;
+    
+        return data;
     }
 }
+
--- src/views/Hardware.vue
+++ src/views/Hardware.vue
@@ -1,28 +1,56 @@
 <template>
   <div>
     <hardwares-list v-bind:hardwares="hardwares"></hardwares-list>
+      <v-pagination
+        v-model="pagination.currentPage"
+        :length="pagination.pageCount"
+        @input="paginationClicked"
+      ></v-pagination>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue';
 import HardwaresList from '@/components/HardwaresList.vue';
 import { HardwaresService } from '@/services/HardwaresService';
 import { HardwareModel } from '@/models/HardwareModel';
+import { PaginationModel } from "@/models/PaginationModel";
+import { PaginationContainer } from "@/models/PaginationContainer";
 
 @Component()
 export default class Hardware extends Vue {
   private hardwaresService!: HardwaresService;
+  private pagination!: PaginationModel;
   private hardwares!: HardwareModel[];
 
-  constructor() {
-    super();
+  beforeCreate() {
     this.hardwaresService = new HardwaresService();
+    this.pagination = new PaginationModel(1, 3, 3);
   }
 
-  public async mounted() {
-    this.hardwares = await this.hardwaresService.getAllHardware();
+  public mounted() {
+    var pageNumber = +this.$route.params.page;
+    if (pageNumber == undefined || isNaN(pageNumber)) {
+      pageNumber = 1;
+    }
+
+    this.pagination.currentPage = pageNumber;
+    this.paginationClicked(pageNumber);
+  }
+
+  public paginationClicked(pageNumber: number) {
+    this.hardwaresService
+      .getHardwareByPage(pageNumber, this.pagination.itemsPerPage)
+      .then((paginationContainer: PaginationContainer<HardwareModel>) => {
+        this.hardwares = paginationContainer.results;
+        this.pagination.itemCount = paginationContainer.count;
+      });
+
+    this.$router.replace({
+      name: "hardware",
+      params: { page: "" + pageNumber }
+    });
   }
 
   public data() {
@@ -30,3 +58,9 @@ export default class Hardware extends Vue {
   }
 }
 </script>
+
+<style>
+.v-pagination .primary {
+  background-color: rgb(65, 65, 255) !important;
+}
+</style>
