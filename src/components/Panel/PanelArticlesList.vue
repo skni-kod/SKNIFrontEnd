ab6227ef3ada7b7c5853fa1a2929449cbe7848a6
tearth <tearthdev@gmail.com>
2019-03-02 19:05:16 +0100

    Add table with articles.

--- src/components/Panel/PanelArticlesList.vue
+++ src/components/Panel/PanelArticlesList.vue
@@ -2,9 +2,20 @@
   <div>
     <v-layout>
       <v-flex xs12 sm6 offset-sm3>
-        <div v-for="article in articles" :key="article.title">
-          <div>{{ article.id }}</div>
-        </div>
+        <v-data-table
+          :headers="headers"
+          :items="articles"
+          class="elevation-1"
+        >
+          <template v-slot:items="props">
+            <td class="text-xs-left">{{ props.item.id }}</td>
+            <td class="text-xs-left"><a v-bind:href="'/#/panel/editarticle/'+ props.item.id">{{ props.item.title }}</a></td>
+            <td class="text-xs-left">{{ props.item.creator.user.username }}</td>
+            <td class="text-xs-left">{{ props.item.creation_date | moment("DD-MM-YYYY HH:mm:SS") }}</td>
+            <td class="text-xs-left">{{ props.item.publication_date | moment("DD-MM-YYYY HH:mm:SS") }}</td>
+            <td class="text-xs-left">{{ props.item.tags.map(p => '#' + p.tag.name).join(', ') }}</td>
+          </template>
+        </v-data-table>
       </v-flex>
     </v-layout>
   </div>
@@ -18,5 +29,18 @@ import { PaginationModel } from "@/models/PaginationModel";
 @Component
 export default class PanelArticlesList extends Vue {
   @Prop() articles!: ArticleModel[];
+  
+  public data() {
+    return {
+      headers: [
+        { text: 'ID', value: 'id' },
+        { text: 'Tytuł', value: 'title' },
+        { text: 'Autor', value: 'creator' },
+        { text: 'Data utworzenia', value: 'creation_date' },
+        { text: 'Data publikacji', value: 'publication_date' },
+        { text: 'Tagi', value: 'tags' },
+      ]
+    }
+  }
 }
 </script>
