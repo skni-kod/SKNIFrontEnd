91d2c6a6ad841152ec596e16ee39dee64688e0dc
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2019-04-03 23:59:09 +0200

    Added Images to Articles Cards, but have no button read more - end of time of day - waiting for next day

--- src/components/SingleArticle.vue
+++ src/components/SingleArticle.vue
@@ -1,7 +1,35 @@
 <template>
     <div v-if='article != undefined'>
       <v-card>
-        <v-card-title primary-title>
+        <v-img
+                class="white--text"
+                height="300px"
+                src="https://www.koty.pl/wp-content/uploads/2013/09/czarny-kot-864x575.jpg"
+        >
+          <v-container fluid>
+            <v-layout fill-height>
+                <span class="headline text-xs-left"><a v-bind:href="'/#/article/'+ article.id + '-' + article.alias">{{ article.title }}</a></span>
+            </v-layout>
+          </v-container>
+          <v-container fluid style="position: absolute; bottom: 0; background-image: linear-gradient(rgba(12, 12, 12, 0), rgba(12, 12, 12, 0.8)); height: 50%;">
+            <div style="position: absolute; bottom: 25%">
+            <div class='text-xs-left'>
+              <v-icon>mdi-account</v-icon>{{article.creator.user.username}}
+              <v-icon>mdi-calendar-today</v-icon>{{article.creation_date | moment('DD-MM-YYYY')}}
+              <span v-if='article.tags.length != 0'>
+                <v-icon>mdi-note</v-icon>
+                <span v-for='(articleTag, index) in article.tags' :key='articleTag.tag.name'>
+                  <a v-bind:href="'/#/tag/'+ articleTag.tag.name">{{ '#' + articleTag.tag.name }}</a>
+                  <span v-if='index != (article.tags.length - 1)'>, </span>
+                </span>
+              </span>
+              <v-icon>mdi-comment</v-icon>{{article.comments_number}}
+            </div>
+            <vue-markdown class='text-xs-left' style='margin-top: 10px;'>{{article.text}}</vue-markdown>
+            </div>
+          </v-container>
+        </v-img>
+        <!--<v-card-title primary-title>
           <div>
             <h3 class='headline mb-0 text-xs-left'>
               <a v-bind:href="'/#/article/'+ article.id + '-' + article.alias">{{ article.title }}</a>
@@ -23,7 +51,7 @@
         </v-card-title>
         <v-card-actions v-if='article.readMore' style='margin-top: -20px;'>
           <v-btn v-bind:href="'/#/article/'+ article.id + '-' + article.alias" flat color='orange'>Czytaj więcej...</v-btn>
-        </v-card-actions>
+        </v-card-actions>-->
       </v-card>
 
       <comments-list v-bind:comments='comments'></comments-list>
--- src/components/homePageComponents/HomeArticleList.vue
+++ src/components/homePageComponents/HomeArticleList.vue
@@ -2,13 +2,12 @@
     <v-container grid-list-md text-xs-center>
         <h2 class="display-2">Najnowsze artykuły</h2>
         <v-layout align-space-between justify-center row fill-height wrap>
-            <v-flex sm12 v-for='(article, index) in articles' :key='article.title' md4 lg4 xl4>
-                    <!--:md12="index === 0"
+            <v-flex sm12 v-for='(article, index) in articles' :key='article.title'
+                    :md12="index === 0"
                     :lg12="index === 0"
                     :xl12="index === 0"
                     :md6="index !== 0"
-                    :lg6="index !== 0"
-            >-->
+                    :lg6="index !== 0">
                 <single-article :article="article"></single-article>
             </v-flex>
         </v-layout>
