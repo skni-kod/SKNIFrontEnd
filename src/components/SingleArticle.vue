2c8f934995f147806ae79f832eed72117163a0e0
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2019-04-07 01:29:57 +0200

    Corrected some things in news card

--- src/components/SingleArticle.vue
+++ src/components/SingleArticle.vue
@@ -6,12 +6,36 @@
                 height="300px"
                 src="https://www.koty.pl/wp-content/uploads/2013/09/czarny-kot-864x575.jpg"
         >
-          <v-container fluid>
+          <div style="height: 100%; width: 100%; display: flex; flex-direction: column">
+          <v-container style="flex: 1 100%">
             <v-layout fill-height>
-                <span class="headline text-xs-left"><a v-bind:href="'/#/article/'+ article.id + '-' + article.alias">{{ article.title }}</a></span>
+                <!--<span class="headline text-xs-left"><a v-bind:href="'/#/article/'+ article.id + '-' + article.alias">{{ article.title }}</a></span>-->
             </v-layout>
           </v-container>
-          <v-container fluid style="position: absolute; bottom: 0; background-image: linear-gradient(rgba(12, 12, 12, 0), rgba(12, 12, 12, 0.8)); height: 50%;">
+
+          <v-container style="background-image: linear-gradient(rgba(12, 12, 12, 0), rgba(12, 12, 12, 0.8)); height: 50%; flex: 1 100%">
+            <v-flex style="text-align: left">
+              <!--<div class='text-xs-left'>
+                <v-icon>mdi-account</v-icon>{{article.creator.user.username}}
+                <v-icon>mdi-calendar-today</v-icon>{{article.creation_date | moment('DD-MM-YYYY')}}
+                <span v-if='article.tags.length != 0'>
+                <v-icon>mdi-note</v-icon>
+                <span v-for='(articleTag, index) in article.tags' :key='articleTag.tag.name'>
+                  <a v-bind:href="'/#/tag/'+ articleTag.tag.name">{{ '#' + articleTag.tag.name }}</a>
+                  <span v-if='index != (article.tags.length - 1)'>, </span>
+                </span>
+              </span>
+                <v-icon>mdi-comment</v-icon>{{article.comments_number}}
+              </div>-->
+              <span class="headline text-xs-left"><a v-bind:href="'/#/article/'+ article.id + '-' + article.alias">{{ article.title }}</a></span>
+              <vue-markdown class='text-xs-left' style='margin-top: 10px;'>{{article.text}}</vue-markdown>
+            </v-flex>
+          </v-container>
+            <v-card-actions v-if='article.readMore' style="background-color: black">
+              <v-btn v-bind:href="'/#/article/'+ article.id + '-' + article.alias" flat color='orange'>Czytaj więcej...</v-btn>
+            </v-card-actions>
+          </div>
+          <!--<v-container fluid style="position: absolute; bottom: 0; background-image: linear-gradient(rgba(12, 12, 12, 0), rgba(12, 12, 12, 0.8)); height: 50%;">
             <div style="position: absolute; bottom: 25%">
             <div class='text-xs-left'>
               <v-icon>mdi-account</v-icon>{{article.creator.user.username}}
@@ -28,6 +52,9 @@
             <vue-markdown class='text-xs-left' style='margin-top: 10px;'>{{article.text}}</vue-markdown>
             </div>
           </v-container>
+          <v-card-actions v-if='article.readMore' style='margin-top: -20px;'>
+            <v-btn v-bind:href="'/#/article/'+ article.id + '-' + article.alias" flat color='orange'>Czytaj więcej...</v-btn>
+          </v-card-actions>-->
         </v-img>
         <!--<v-card-title primary-title>
           <div>
