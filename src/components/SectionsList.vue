77c944b2aee1092ba357e72750c383f171d5b3d4
Kamil <urlikodd@gmail.com>
2020-08-03 14:18:42 +0200

    Redesigned & fixed sections list

--- src/components/SectionsList.vue
+++ src/components/SectionsList.vue
@@ -1,28 +1,14 @@
 <template>
   <div>
     <v-container fluid>
-      <v-row wrap justify="center">
-        <v-flex xs12 md7 v-for="section in sections" :key="section.title">
-          <v-card
-            class="section ma-2"
-            style="border: 1px solid gray"
-          >
+      <v-row wrap>
+        <v-flex xs12 v-for="section in sections" :key="section.title">
+          <v-card class="mx-auto my-2 bg-gradient" dark max-width="800">
             <v-card-title>
-              <p class="section-title">
-                <v-icon x-large class="blue--text text--lighten-2">{{section.icon}}</v-icon>
-                <a>{{ section.name }}</a>
-              </p>
+              <v-icon large left>{{ section.icon }}</v-icon>
+              <span class="title break font-weight-bold">{{ section.name }}</span>
             </v-card-title>
-            <v-card-text>
-              <div class="text">
-                <p class="section-description">{{ section.description }}</p>
-              </div>
-            </v-card-text>
-            <div class="section-button">
-              <!-- <v-btn  text small color="#64b5f6" class="font-weight-bold button" >
-              Czytaj wiecej...
-              </v-btn>-->
-            </div>
+            <v-card-text class="headline">{{ section.description }}</v-card-text>
           </v-card>
         </v-flex>
       </v-row>
@@ -38,41 +24,12 @@ export default class SectionsList extends Vue {
   @Prop() public sections!: SectionModel[];
 }
 </script>
+
 <style <style lang="scss" scoped>
-.section {
-  background-image: linear-gradient(
-      to bottom,
-      rgba(12, 12, 12, 0),
-      rgba(12, 12, 12, 0.8)
-    ),
-    url("../assets/strona_bg.png");
-  background-size: 100% 100%;
-  border-style: none !important;
-  position: relative;
+.bg-gradient {
+  background: linear-gradient(#aecad5, #2e5d71);
 }
-.section-title {
-  color: white;
-  text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
-}
-.text {
-  display: flex;
-  justify-content: center;
-  color: white;
-  text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
-}
-.section-description {
-  width: 90%;
-  text-align: justify;
-}
-.section-button {
-  position: absolute;
-  bottom: 0px;
-  right: 0px;
-  .button {
-    margin: 0px 55px 30px 0;
-  }
-}
-a:hover {
-  text-decoration: underline;
+.break {
+  word-break: break-word;
 }
 </style>
