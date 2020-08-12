bf212e3e00707e733b1a2f842190bd2d6b1b776b
Kamil <urlikodd@gmail.com>
2020-08-12 23:58:54 +0200

    Fixed icon to left of the title

--- src/components/SectionsList.vue
+++ src/components/SectionsList.vue
@@ -5,8 +5,21 @@
         <v-flex xs12 v-for="section in sections" :key="section.title">
           <v-card class="mx-auto my-2 bg-gradient" dark max-width="800">
             <v-card-title>
-              <v-icon large left>{{ section.icon }}</v-icon>
-              <span class="title break font-weight-bold">{{ section.name }}</span>
+              <v-row class="mx-auto">
+                <v-col cols="auto" class="text-center py-0">
+                  <v-row justify="center" class="flex-column ma-0 fill-height">
+                    <v-icon x-large left>{{ section.icon }}</v-icon>
+                  </v-row>
+                </v-col>
+                <v-col class="pa-0">
+                  <v-row justify="center" class="flex-column ma-0 fill-height">
+                    <p
+                      class="text-left text-h6 font-weight-bold my-auto"
+                      style="word-break: break-word;"
+                    >{{ section.name }}</p>
+                  </v-row>
+                </v-col>
+              </v-row>
             </v-card-title>
             <v-card-text class="headline">{{ section.description }}</v-card-text>
           </v-card>
@@ -28,8 +41,5 @@ export default class SectionsList extends Vue {
 <style <style lang="scss" scoped>
 .bg-gradient {
   background: linear-gradient(#aecad5, #2e5d71);
-}
-.break {
-  word-break: break-word;
 }
 </style>
