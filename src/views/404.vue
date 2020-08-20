64b5813a77c0ee5b015ef9d44a41ae8f64d702a9
Kamil <urlikodd@gmail.com>
2020-08-20 23:35:01 +0200

    Fixed text positioning

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,24 +1,18 @@
 <template>
   <v-row align="center" style="height: 100%;">
     <v-col cols="12">
-      <h1 class="text-h1 font-weight-black">404</h1>
-      <h1 class="text-h2 font-weight-bold">Strona nie istnieje!</h1>
-      <v-container class="pt-5 px-1">
-        <v-layout row wrap justify-space-between>
-          <v-flex xs12 :md6="$router.history.length > 0" class="mx-2">
-            <v-btn class="primary" router :to="{name: 'home'}">
-              <v-icon left>mdi-home</v-icon>
-              <span>Powrót na stronę główną</span>
-            </v-btn>
-          </v-flex>
-          <v-flex v-if="$router.history.length > 0" xs12 md6 class="mx-2">
-            <v-btn class="primary" @click="$router.go(-1);">
-              <v-icon left>mdi-keyboard-return</v-icon>
-              <span>Powrót na poprzednią stronę</span>
-            </v-btn>
-          </v-flex>
-        </v-layout>
-      </v-container>
+      <p class="text-h1 text-center font-weight-black">404</p>
+      <p class="text-h2 text-center font-weight-bold">Strona nie istnieje!</p>
+      <v-row align="center" justify="space-around" class="my-5">
+        <v-btn class="primary my-2" router :to="{name: 'home'}">
+          <v-icon left>mdi-home</v-icon>
+          <span>Powrót na stronę główną</span>
+        </v-btn>
+        <v-btn v-if="$router.history.length > 0" class="primary my-2" @click="$router.go(-1);">
+          <v-icon left>mdi-keyboard-return</v-icon>
+          <span>Powrót na poprzednią stronę</span>
+        </v-btn>
+      </v-row>
     </v-col>
   </v-row>
 </template>
