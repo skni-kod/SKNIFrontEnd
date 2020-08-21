9c6ac559671cad4c575e970452a3b2440677a94e
Kamil <urlikodd@gmail.com>
2020-08-21 13:50:30 +0200

    Added new Login UI

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,30 +1,49 @@
 <template>
-  <v-container>
-    <v-layout row class="text-xs-center">
-      <v-flex xs4 class="grey lighten-4">
-        <v-container class="text-xs-center">
-          <v-card text>
-            <v-card-title primary-title>
-              <h4>Login</h4>
-            </v-card-title>
-            <v-form>
-              <v-text-field prepend-icon="person" name="Username" label="Username" v-model="login"></v-text-field>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm10 md8 lg6 xl4>
+        <v-card class="elevation-12">
+          <v-toolbar color="primary">
+            <v-toolbar-title class="white--text font-weight-bold">Zaloguj się</v-toolbar-title>
+          </v-toolbar>
+          <v-form v-model="inputValidated" @submit.prevent="loginUser()">
+            <v-card-text>
               <v-text-field
-                prepend-icon="lock"
-                name="Password"
-                label="Password"
-                type="password"
-                v-model="password"
+                outlined
+                hide-details
+                v-model="login"
+                prepend-icon="mdi-account"
+                label="Login"
+                color="primary"
+                type="text"
+                class="my-2"
               ></v-text-field>
-              <v-card-actions>
-                <v-btn primary large block @click="loginUser()">Login</v-btn>
-              </v-card-actions>
-              <v-card-actions>
-                <v-btn primary large block @click="goToRegister()">Register</v-btn>
-              </v-card-actions>
-            </v-form>
-          </v-card>
-        </v-container>
+              <v-text-field
+                outlined
+                hide-details
+                v-model="password"
+                prepend-icon="mdi-lock"
+                :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPass = !showPass"
+                label="Hasło"
+                color="primary"
+                :type="showPass ? 'text' : 'password'"
+                class="my-2"
+              ></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                :disabled="!inputValidated || !login.length >= 3 || !password.length >= 8"
+                color="primary"
+                type="submit"
+              >
+                <span class="font-weight-bold">Zaloguj</span>
+                <v-icon right>mdi-key</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -46,6 +65,15 @@ export default class Login extends Vue {
     if (res.status === 200) {
       this.$forceUpdate();
     }
+  }
+
+  private data() {
+    return {
+      inputValidated: false,
+      login: '',
+      password: '',
+      showPass: false,
+    };
   }
 }
 </script>
