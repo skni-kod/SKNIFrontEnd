ecc5ab2fbaeef02086162ae251960182d9ecba2d
Kamil <urlikodd@gmail.com>
2020-09-17 23:44:58 +0200

    Added parsing readmore guard

--- src/services/ProjectsService.ts
+++ src/services/ProjectsService.ts
@@ -3,6 +3,8 @@ import { PaginationContainer } from '@/models/PaginationContainer';
 import axios from '../axios';
 
 export class ProjectsService {
+    private readMoreGuard = '---readmore---';
+
     public async getAllProjects(): Promise<ProjectModel[]> {
         return (await axios('api/project/', {
             params: {
@@ -11,12 +13,21 @@ export class ProjectsService {
         })).data;
     }
 
-    public async getProject(projectNumber: number): Promise<ProjectModel> {
-        return (await axios('api/project/' + projectNumber, {
+    public async getProject(projectNumber: number, removeReadMore: boolean = true): Promise<ProjectModel> {
+        const project = (await axios('api/project/' + projectNumber, {
             params: {
                 format: 'json',
             },
         })).data;
+
+        if (removeReadMore) {
+            const readMoreIndex = project.text.indexOf(this.readMoreGuard);
+            if (readMoreIndex !== -1) {
+                project.text = project.text.replace(this.readMoreGuard, '');
+            }
+        }
+
+        return project;
     }
 
     public async getProjectsByPage(pageNumber: number, pageSize: number): Promise<PaginationContainer<ProjectModel>> {
