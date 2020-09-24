3c478f9ab2e5c1c9f53c4862e4343732533451b4
Kamil <urlikodd@gmail.com>
2020-09-24 19:45:25 +0200

    Added link validation
    Thanks to @dperini
    https://gist.github.com/dperini/729294

--- src/components/LinkListInput.vue
+++ src/components/LinkListInput.vue
@@ -12,7 +12,7 @@
             <v-text-field
               clearable
               v-model="item.text"
-              :rules="[required]"
+              :rules="[required, isValidLink]"
               :label="'Link ' + (i + 1)"
             ></v-text-field>
           </v-col>
@@ -112,6 +112,36 @@ export default class LinkListInput extends Vue {
         },
       ],
       required: (value: string) => !!value || 'Pole wymagane',
+      isValidLink: (value: string) => {
+        // https://gist.github.com/dperini/729294
+        const pattern = new RegExp(
+          '^' +
+            '(?:(?:(?:https?|ftp):)?\\/\\/)' +
+            '(?:\\S+(?::\\S*)?@)?' +
+            '(?:' +
+            '(?!(?:10|127)(?:\\.\\d{1,3}){3})' +
+            '(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})' +
+            '(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})' +
+            '(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])' +
+            '(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}' +
+            '(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))' +
+            '|' +
+            '(?:' +
+            '(?:' +
+            '[a-z0-9\\u00a1-\\uffff]' +
+            '[a-z0-9\\u00a1-\\uffff_-]{0,62}' +
+            ')?' +
+            '[a-z0-9\\u00a1-\\uffff]\\.' +
+            ')+' +
+            '(?:[a-z\\u00a1-\\uffff]{2,}\\.?)' +
+            ')' +
+            '(?::\\d{2,5})?' +
+            '(?:[/?#]\\S*)?' +
+            '$',
+          'i',
+        );
+        return pattern.test(value) || 'Link nie jest poprawny';
+      },
     };
   }
 }
