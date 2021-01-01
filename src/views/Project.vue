00a4bda7ee121b7a37ee6b5db5ffc8d8bbeab864
Kamil Klecha <urlikodd@gmail.com>
2021-01-01 15:49:03 +0100

    Moved SingleProject component into Project view

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,5 +1,65 @@
 <template>
-  <single-project :project="project"></single-project>
+  <div>
+    <v-row justify="center" v-if="project != undefined">
+      <v-col cols="12" sm="10" md="8" lg="6" xl="4">
+        <v-card tile elevation="0">
+          <v-card-title style="word-break: break-word">
+            <p class="text-h2 text-left mb-0">{{ project.title }}</p>
+          </v-card-title>
+          <v-card-subtitle class="text-left pb-0">
+            <v-chip label>
+              <v-avatar left>
+                <v-icon>mdi-account-circle</v-icon>
+              </v-avatar>
+              <span>{{ project.creator.user.username }}</span>
+            </v-chip>
+            <p class="mb-0">
+              Data publikacji:
+              <b>{{
+                project.publication_date | moment('YYYY-MM-DD HH:mm:ss')
+              }}</b>
+            </p>
+          </v-card-subtitle>
+          <v-card-actions v-if="project.repository_link" class="py-0 ml-2">
+            <v-btn
+              class="purple darken-2 white--text"
+              :href="project.repository_link"
+              target="_blank"
+            >
+              <v-icon left class>mdi-github</v-icon>
+              <span>Repozytorium</span>
+            </v-btn>
+            <v-spacer />
+          </v-card-actions>
+          <v-card-text>
+            <markdown-it-vue
+              class="md-body text-left"
+              :content="project.text"
+              :options="markdownOptions"
+            />
+          </v-card-text>
+        </v-card>
+        <gallery breakpoints="xs6" :imgs="project.gallery" />
+      </v-col>
+    </v-row>
+    <v-row justify="center" v-if="project != undefined">
+      <v-col cols="12" sm="10" md="8" lg="6" xl="4" class="text-center">
+        <p>
+          Made with
+          <v-icon color="red">mdi-heart</v-icon> by
+        </p>
+        <div v-for="author in project.authors" :key="author.user.username">
+          <p>
+            <b>{{ author.user.first_name }} {{ author.user.last_name }}</b>
+          </p>
+        </div>
+        <p>
+          at
+          <b>{{ project.section.name }}</b>
+        </p>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang='ts'>
@@ -17,16 +77,28 @@ export default class Project extends Vue {
   }
 
   private mounted() {
-    this.projectsService.getProject(+this.$route.params.id).then((project) => {
-      this.project = project;
-    }).catch(() => {
-      this.$router.replace('/404');
-    });
+    this.projectsService
+      .getProject(+this.$route.params.id)
+      .then((project) => {
+        this.project = project;
+      })
+      .catch(() => {
+        this.$router.replace('/404');
+      });
   }
 
   private data() {
     return {
       project: this.project,
+      markdownOptions: {
+        markdownIt: {
+          html: true,
+          linkify: true,
+        },
+        githubToc: {
+          anchorLink: false,
+        },
+      },
     };
   }
 }
