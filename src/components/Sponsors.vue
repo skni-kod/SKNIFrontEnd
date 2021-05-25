f09dbedf811db81fae879e7720eb5fbd61506239
Kamil Klecha <urlikodd@gmail.com>
2021-05-25 14:53:22 +0200

    Fixed component

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -1,17 +1,18 @@
 <template>
-    <div>
-        <v-carousel 
-        cycle
-        height="500"
-        hide-delimiter-background
-        show-arrows-on-hover>
-            <v-carousel-item
-            v-for="(item,i) in items"
-            :key="i"
-            :src="item.src"
-            ></v-carousel-item>
-        </v-carousel>
-    </div>
+  <div>
+    <v-carousel
+      cycle
+      height="500"
+      hide-delimiter-background
+      show-arrows-on-hover
+    >
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 
 <script lang="ts">
@@ -20,29 +21,29 @@ import { Component } from 'vue-property-decorator';
 
 @Component
 export default class Sponsors extends Vue {
-
-     data () {
-      return {
-        items: [
-          {
-            src: 'C:\Users\Dawid\Desktop\SKNIFront\SKNIFrontEnd\src\assets\logo_color.png',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-          },
-          {
-            src: 'https://news.clemson.edu/wp-content/uploads/2020/12/Deloitte-Logo-FEATURE.jpg',
-          },
-          {
-            src: 'https://www.ideoagency.com/gfx/ideo-2015/en/ogimage/logo_ideo_kolor_invertrgb_kwadrat.png',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg', 
-          },
-        ],
-      }
-    },
-    
-  
+  private data() {
+    return {
+      items: [
+        {
+          src:
+            'C:UsersDawidDesktopSKNIFrontSKNIFrontEndsrcassetslogo_color.png',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src:
+            'https://news.clemson.edu/wp-content/uploads/2020/12/Deloitte-Logo-FEATURE.jpg',
+        },
+        {
+          src:
+            'https://www.ideoagency.com/gfx/ideo-2015/en/ogimage/logo_ideo_kolor_invertrgb_kwadrat.png',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    };
+  }
 }
 </script>
