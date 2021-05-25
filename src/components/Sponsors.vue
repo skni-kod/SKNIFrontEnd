cd8ed1aaef697d19e9e9c20062881a93445718b9
Dawid Potocki <dpotocki57@gmail.com>
2021-05-25 13:57:11 +0200

    add sponsor

---
+++ src/components/Sponsors.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <v-carousel 
+        cycle
+        height="500"
+        hide-delimiter-background
+        show-arrows-on-hover>
+            <v-carousel-item
+            v-for="(item,i) in items"
+            :key="i"
+            :src="item.src"
+            ></v-carousel-item>
+        </v-carousel>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class Sponsors extends Vue {
+
+     data () {
+      return {
+        items: [
+          {
+            src: 'C:\Users\Dawid\Desktop\SKNIFront\SKNIFrontEnd\src\assets\logo_color.png',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://news.clemson.edu/wp-content/uploads/2020/12/Deloitte-Logo-FEATURE.jpg',
+          },
+          {
+            src: 'https://www.ideoagency.com/gfx/ideo-2015/en/ogimage/logo_ideo_kolor_invertrgb_kwadrat.png',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg', 
+          },
+        ],
+      }
+    },
+    
+  
+}
+</script>
