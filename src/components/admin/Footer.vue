abef6d1800787185917b957156f4c2c9d495272e
Adrian <dobryemail0@wp.pl>
2021-07-02 19:15:52 +0200

    Added CRUD for footer links in admin panel

--- src/components/admin/Footer.vue
+++ src/components/admin/Footer.vue
@@ -1,15 +1,183 @@
 <template>
-  <div></div>
+  <div>
+    <v-row align="center" class="fill-height">
+      <v-col>
+        <v-row
+          no-gutters
+          align="center"
+          justify="center"
+          class="ma-2 fill-height"
+        >
+          <v-col lg="9">
+            <footer-links-list
+              :list="links"
+              @remove="deleteLink($event)"
+              @edit="showEditForm($event)"
+              >Test</footer-links-list
+            >
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-btn fab fixed bottom right class="success" @click="addNew = true">
+      <v-icon large>mdi-plus</v-icon>
+    </v-btn>
+    <v-form v-if="addNew || edit">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="6">
+            <v-text-field
+              label="Tytuł"
+              counter="128"
+              v-model="linkForm.title"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field
+              label="Adres URL"
+              v-model="linkForm.link"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field
+              label="Ikonka (mdi-)(https://materialdesignicons.com)"
+              counter="64"
+              v-model="linkForm.icon"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field
+              v-model="linkForm.color"
+              label="Kolor"
+              counter="64"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-row justify="end" class="my-3">
+            <v-btn
+              color="success"
+              class="mr-4"
+              @click="addLink()"
+              v-if="addNew"
+            >
+              Dodaj link
+            </v-btn>
+            <v-btn
+              color="success"
+              class="mr-4"
+              @click="editLink()"
+              v-if="edit"
+            >
+              Edytuj link
+            </v-btn>
+            <v-btn
+              color="error"
+              class="mr-4"
+              @click="
+                addNew = false;
+                edit = false;
+                linkForm = { title: '', link: '', icon: '', color: '' };
+              "
+            >
+              Anuluj
+            </v-btn>
+          </v-row>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import FooterLinksList from './FooterLinksList.vue';
+import { FooterService } from '@/services/FooterService';
 import beAxios from '@/axios';
 
-@Component
+@Component({
+  components: {
+    FooterLinksList,
+    NewTag: () =>
+      import(/* webpackChunkName: "ComponentNewTag" */ './AddTag.vue'),
+  },
+})
 export default class AdminPanelFooter extends Vue {
+  private footerService!: FooterService;
+
+  private beforeCreate() {
+    this.footerService = new FooterService();
+  }
+
+  private created() {
+    this.getLinks();
+  }
+
+  private getLinks() {
+    this.footerService
+      .getLinks()
+      .then((res) => {
+        this.$data.links = res;
+      })
+      .catch(() => {
+        this.$store.dispatch('errorMessage', 'Błąd przy pobieraniu linków!');
+      });
+  }
+
+  private addLink() {
+    this.footerService
+      .addLink(this.$data.linkForm)
+      .then(() => {
+        this.getLinks();
+        this.$store.dispatch('successMessage', 'Link został dodany');
+      })
+      .catch(() => {
+        this.$store.dispatch('errorMessage', 'Błąd przy dodawaniu linku!');
+      });
+    this.$data.newTagName = '';
+    this.$data.addNew = false;
+  }
+  showEditForm(data: any) {
+    this.$data.edit = true;
+    this.$data.linkForm = data;
+  }
+  private editLink() {
+    this.footerService
+      .editLink(this.$data.linkForm.id,this.$data.linkForm)
+      .then(() => {
+        this.getLinks();
+        this.$store.dispatch('successMessage', 'Link został zmieniony');
+        this.$store.dispatch('infoMessage', 'Zmiany będą widoczne po odświeżeniu strony');
+      })
+      .catch(() => {
+        this.$store.dispatch('errorMessage', 'Błąd przy edycji linku!');
+      });
+    this.$data.newTagName = '';
+    this.$data.edit = false;
+  }
+
+  private deleteLink(id: number) {
+    this.footerService
+      .deleteLink(id)
+      .then(() => {
+        this.getLinks();
+        this.$store.dispatch('successMessage', 'Link został usunięty');
+      })
+      .catch(() => {
+        this.$store.dispatch('errorMessage', 'Błąd przy usuwaniu linku!');
+      });
+  }
+
   private data() {
-    return {};
+    return {
+      addNew: false,
+      edit: false,
+      links: [],
+      validate: false,
+      linkForm: { title: '', link: '', icon: '', color: '' },
+    };
   }
 }
 </script>
---
+++ src/components/admin/FooterLinksList.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-card outlined class="rounded-lg">
+    <v-card-title
+      class="text-h4 white--text justify-center font-weight-bold primary pa-0"
+      >Lista linków</v-card-title
+    >
+    <v-card-text class="pa-2" v-if="list.length > 0">
+      <v-row align="center" justify="center" class="ma-2">
+        <v-col
+          cols="12"
+          sm="6"
+          lg="4"
+          xl="3"
+          v-for="el in list"
+          :key="el.id"
+          class="pa-1"
+        >
+          <v-hover v-slot:default="{ hover }">
+            <v-card class="px-2">
+              <v-card-text>
+                <v-row align="center" justify="center">
+                  <v-col cols="auto" class="pa-0">
+                    <v-icon x-large :color="el.color">{{ el.icon }}</v-icon>
+                  </v-col>
+                  <v-col cols="auto" class="pa-0">
+                    <v-card-title
+                      class="text-h4 font-weight-thin justify-center"
+                      >{{ el.title }}</v-card-title
+                    >
+                  </v-col>
+                </v-row>
+                <v-row align="center" justify="center">
+                  <v-col cols="auto">
+                    {{ el.link }}
+                  </v-col>
+                </v-row>
+                <v-fade-transition>
+                  <v-overlay
+                    v-if="hover"
+                    absolute
+                    opacity="0.8"
+                    color="success"
+                    @click.native="edit(el)"
+                    style="cursor: pointer; width: 50%"
+                  >
+                    <v-row
+                      no-gutters
+                      class="fill-height"
+                      align="center"
+                      justify="center"
+                    >
+                      <v-col cols="auto">
+                        <v-icon x-large class="white--text pa-0 ma-0"
+                          >mdi-pencil</v-icon
+                        >
+                      </v-col>
+                    </v-row>
+                  </v-overlay>
+                </v-fade-transition>
+                <v-fade-transition>
+                  <v-overlay
+                    v-if="hover"
+                    absolute
+                    opacity="0.8"
+                    color="error"
+                    @click.native="remove(el.id)"
+                    style="cursor: pointer; width: 50%; left: 50%"
+                  >
+                    <v-row
+                      no-gutters
+                      class="fill-height"
+                      align="center"
+                      justify="center"
+                    >
+                      <v-col cols="auto">
+                        <v-icon x-large class="white--text pa-0 ma-0"
+                          >mdi-delete</v-icon
+                        >
+                      </v-col>
+                    </v-row>
+                  </v-overlay>
+                </v-fade-transition>
+              </v-card-text>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </v-row>
+    </v-card-text>
+    <v-card-text class="text-h3 text-center my-2" v-else
+      >Brak linków! Dodaj jakiś :)</v-card-text
+    >
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import FooterModel from '@/models/FooterModel.vue';
+
+@Component
+export default class FooterLinksList extends Vue {
+  @Prop({ required: true }) private list!: object;
+
+  private edit(link:any) {
+    this.$emit('edit', link);
+  }
+  private remove(id: number) {
+    this.$emit('remove', id);
+  }
+  private removeClicked(name: string) {
+    this.$emit('remove', name);
+  }
+}
+</script>
