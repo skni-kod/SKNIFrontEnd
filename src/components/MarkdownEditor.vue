7d6c15f3154ec3905f00092ebbe418f025717eae
Kamil Klecha <urlikodd@gmail.com>
2021-07-03 15:55:23 +0200

    Improved markdown editor

--- src/components/MarkdownEditor.vue
+++ src/components/MarkdownEditor.vue
@@ -2,12 +2,12 @@
   <div>
     <v-expansion-panels dark class="my-2">
       <v-expansion-panel class="white--text info">
-        <v-expansion-panel-header class="text-h6 font-weight-bold py-1"
+        <v-expansion-panel-header class="text-h5 font-weight-bold py-1"
           >Wyświetl ważne informacje dotyczące edytora</v-expansion-panel-header
         >
         <v-expansion-panel-content>
           <ul>
-            <li class="text-body-1">
+            <li class="text-h6">
               Poniższy edytor tekstu obsługuje język znaczników Markdown. Jego
               podstawy możesz sprawdzić
               <b>
@@ -19,9 +19,24 @@
                 > </b
               >.
             </li>
+            <li class="text-body-1">
+              Przykładowy markdown, który obsługuje ten edytor, możesz znaleźć
+              <b>
+                <a
+                  href="http://www.aqcoder.com/markdown"
+                  style="color: inherit"
+                  target="_blank"
+                  >tutaj</a
+                > </b
+              >.
+            </li>
             <li>
-              Edytor obsługuje składnię html, ale nie jest ona aktywna w widoku
-              listy artykułów / projektów.
+              Edytor obsługuje składnię html, ale jest ona aktywna
+              <b>tylko</b> w widoku pojedyncaego artykułu, projektu lub sekcji.
+            </li>
+            <li>
+              Jeżeli tekst zawiera obrazki, podglad należy odświeżać ręcznie
+              przyciskiem powyżej podglądu.
             </li>
             <li>
               Do
@@ -30,11 +45,13 @@
               zostanie wyświetlony i będzie error na konsoli.
             </li>
             <li>
-              Podczas edycji artykułu oraz projektu, należy do tekstu dodać
-              znacznik ---readmore---, aby podgląd artykułu na liście artykułów
-              nie był zbyt długi. Można stosować składnię języka markdown przed
-              znacznikiem ---readmore---. Na liście artykułów / projektów
-              skladnia ta zostanie usunięta.
+              <b
+                >Podczas edycji artykułu oraz projektu, należy do tekstu dodać
+                znacznik ---readmore---</b
+              >, aby podgląd artykułu lub projektu na liście nie był zbyt długi.
+              Można stosować składnię języka markdown przed tym znacznikiem.
+              Podczas wyświetlania listy artykułów / projektów skladnia ta
+              zostanie usunięta.
             </li>
           </ul>
         </v-expansion-panel-content>
@@ -52,6 +69,15 @@
         ></v-textarea>
       </v-col>
       <v-col cols="12" md="6" class="pa-1">
+        <v-btn-cap
+          block
+          class="primary mb-2"
+          @click="textUpdated(markdown, '', true)"
+          v-if="enableHotReload"
+        >
+          <span>Przeładuj podgląd</span>
+          <v-icon right>mdi-reload</v-icon>
+        </v-btn-cap>
         <v-card outlined>
           <v-card-title
             class="primary text-h6 white--text font-weight-bold py-1"
@@ -60,7 +86,7 @@
           <markdown-it-vue
             class="md-body text-left mx-2"
             :content="
-              markdown || '## <center>Nie wprowadzono żadnego tekstu!</cewnter>'
+              preview || '## <center>Nie wprowadzono żadnego tekstu!</cewnter>'
             "
             :options="markdownOptions"
           />
@@ -71,7 +97,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Watch, Vue } from 'vue-property-decorator';
 
 @Component
 export default class MarkdownEditor extends Vue {
@@ -87,8 +113,26 @@ export default class MarkdownEditor extends Vue {
     this.$emit('input', text);
   }
 
+  private containImg(text: string) {
+    const regex = /!\[[^\]]*\]\([^)]+\)/i;
+    return regex.test(text);
+  }
+
+  get enableHotReload() {
+    return this.containImg(this.markdown);
+  }
+
+  @Watch('value')
+  private textUpdated(newVal: string, oldVal: string, upd: boolean = false) {
+    if (!this.enableHotReload || upd) {
+      this.$data.preview = newVal;
+    }
+  }
+
   private data() {
     return {
+      preview: undefined,
+      reload: false,
       required: (value: string) => !!value || 'Pole wymagane',
       markdownOptions: {
         markdownIt: {
